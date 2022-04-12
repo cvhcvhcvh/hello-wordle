39a7154854002e99d84ff4fd886c3adfb58c3feb
Chase <chasevanhaselen@gmail.com>
2022-04-11 22:42:58 -0500

    removedcomments

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -2,8 +2,6 @@ import { useState } from "react";
 
 const Alert = ({ alert, type }) => {
 
-  console.log("alert is", alert)
-
   return (
     <div className="alert-container">
       <div className={alert ? "alert" : "alert hidden"}>{type}</div>
